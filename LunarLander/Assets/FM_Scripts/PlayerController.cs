5b79cf9cd624da9e7b551a6e2c848712049dba5c
pandaDaoist <42003923+pandaDaoist@users.noreply.github.com>
2018-12-01 20:06:23 -0500

    Checkpoint01

---
+++ LunarLander/Assets/FM_Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Text scoreText, timeText, splashText;
+
+	// Use this for initialization
+	void Start () {
+        splashText.text = "LAND!";
+        scoreText.text = "0";
+        timeText.text = "10";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private Counter ()
+    {
+
+    }
+}
