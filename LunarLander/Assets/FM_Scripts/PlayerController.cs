616b60960498f3c20f677f1c2f786407c7f5cc2b
pandaDaoist <42003923+pandaDaoist@users.noreply.github.com>
2018-12-01 20:31:39 -0500

    Controller Problem

--- LunarLander/Assets/FM_Scripts/PlayerController.cs
+++ LunarLander/Assets/FM_Scripts/PlayerController.cs
@@ -6,21 +6,45 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour {
 
     private Text scoreText, timeText, splashText;
+    private int timer;
+    private Rigidbody2D rb2d;
+
+    public int speed;
 
 	// Use this for initialization
 	void Start () {
         splashText.text = "LAND!";
         scoreText.text = "0";
         timeText.text = "10";
+        timer = 0;
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        if (timer >= 10)
+        {
+            splashText.text = "You Lose! :(";
+            StartCoroutine(ByeAfterDelay(2));
 
-    private Counter ()
-    {
+        }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+            Application.Quit();
 
     }
+
+    IEnumerator ByeAfterDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+        //GameLoader.gameOn = false;
+    }
+
+
 }
